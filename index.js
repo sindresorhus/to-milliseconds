b4977d9ec9e3cfa31960edf56f1fc62e0cded8c5
Hyan Mandian <hyanmandian@hotmail.com>
2018-07-04 13:48:07 -0300

    Replace for loop with reduce and move switch logic to object

--- index.js
+++ index.js
@@ -1,39 +1,23 @@
 'use strict';
 
-module.exports = object => {
-	let ms = 0;
+const transformers = {
+	days: value => value * 864e5,
+	hours: value => value * 36e5,
+	minutes: value => value * 6e4,
+	seconds: value => value * 1e3,
+	milliseconds: value => value,
+	microseconds: value => value / 1e3,
+	nanoseconds: value => value / 1e6
+};
 
-	for (const [key, value] of Object.entries(object)) {
-		if (typeof value !== 'number') {
-			throw new TypeError(`Expected a \`number\` for key \`${key}\`, got \`${value}\` (${typeof value})`);
-		}
-
-		switch (key) {
-			case 'days':
-				ms += value * 864e5;
-				break;
-			case 'hours':
-				ms += value * 36e5;
-				break;
-			case 'minutes':
-				ms += value * 6e4;
-				break;
-			case 'seconds':
-				ms += value * 1e3;
-				break;
-			case 'milliseconds':
-				ms += value;
-				break;
-			case 'microseconds':
-				ms += value / 1e3;
-				break;
-			case 'nanoseconds':
-				ms += value / 1e6;
-				break;
-			default:
-				throw new Error('Unsupported time key');
-		}
+module.exports = object => Object.entries(object).reduce((ms, [key, value]) => {
+	if (typeof value !== 'number') {
+		throw new TypeError(`Expected a \`number\` for key \`${key}\`, got \`${value}\` (${typeof value})`);
 	}
 
-	return ms;
-};
+	if (!transformers[key]) {
+		throw new Error('Unsupported time key');
+	}
+
+	return ms + transformers[key](value);
+}, 0);

57d4534e6ac8a8928c9ffb13acc47c77b0bd819b
luan0ap <luanap.kun@gmail.com>
2018-07-12 15:02:59 -0300

    Removed all switch condition, just formted to functional

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 'use strict'
 
-module.exports = object => {
-	let ms = 0
+const unity = {
+	days: val => (val * 864e5),
+	hours: val => (val * 36e5),
+	minutes: val => (val * 6e4),
+	seconds: val => (val * 1e3),
+	milliseconds: val => (val),
+	microseconds: val => (val / 1e3),
+	nanoseconds: val => (val / 1e6)
+}
 
+module.exports = object => {
 	for (const [key, value] of Object.entries(object)) {
 		if (typeof value !== 'number') {
 			throw new TypeError(`Expected a \`number\` for key \`${key}\`, got \`${value}\` (${typeof value})`)
 		}
 
-		switch (key) {
-			case 'days':
-				ms += value * 864e5
-				break
-			case 'hours':
-				ms += value * 36e5
-				break
-			case 'minutes':
-				ms += value * 6e4
-				break
-			case 'seconds':
-				ms += value * 1e3
-				break
-			case 'milliseconds':
-				ms += value
-				break
-			case 'microseconds':
-				ms += value / 1e3
-				break
-			case 'nanoseconds':
-				ms += value / 1e6
-				break
-			default:
-				throw new Error('Unsupported time key')
-		}
+		return unity[key](value)
 	}
-
-	return ms
 }
